997c16f5459bd677015be5c1a123c695e4e4b886
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-07-02 23:51:32 -0400

    initial creation

---
+++ src/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("palabraita").style.display = "none";
+});
+
+function palabraita() {
+    document.getElementById("main_screen").style.display = "none";
+    document.getElementById("palabraita").style.display = "block";
+}
