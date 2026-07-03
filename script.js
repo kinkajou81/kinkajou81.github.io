d731f418fc7789fda7a6785606e175b7fb060015
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-07-03 15:17:08 -0400

    finished palabraita start screen

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function palabraita() {
     document.getElementById("main_screen").style.display = "none";
     document.getElementById("palabraita").style.display = "block";
 }
+
+function palabraita_play() {
+    document.getElementById("main_screen").style.display = "none";
+    document.getElementById("palabraita").style.display = "none";
+    document.getElementById("palabraita_play").style.display = "block";
+}
